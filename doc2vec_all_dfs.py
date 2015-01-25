d95239008345f786d12a1a8306bc13f10edc07af
David Hochstetler <dlhochstetler@gmail.com>
2015-01-25 14:07:21 -0800

    doc2vec_all_dfs.py loading and processing all lemmatized dfs. However, this version is based on a for i loop that kicked out an error because df_11 does not exist.

--- doc2vec_all_dfs.py
+++ doc2vec_all_dfs.py
@@ -8,10 +8,22 @@ David L. Hochstetler
 """
 
 import doc2vec_methods as dm
+import pandas as pd
 import cPickle as pickle
+import time
+from doc2vec_classes import LabeledReviewSentence
+import gensim, logging 		# For Doc2Vec
+import os
 
 
-def main():
+
+# # Pickle and save the adjust df
+# new_file = 'df_reviews_' +str(i) +'.pandas'
+# print("Saving pickled dataframe as " + new_file)
+# new_file_path = directory_path + new_file
+# pickle.dump( df, open( new_file_path, "wb"))
+
+if __name__ == '__main__':
 	# Set the path to the pickled file
 	directory_path = '../../Lemmatized_by_Sentence/'
 	# Loop through each df, convert to desired format, and pickle the new df
@@ -19,18 +31,56 @@ def main():
 	file_path_base = 'amazon_music_random_lemmatized_'
 	file_path_end = '.pandas'
 	number_dfs = 22
+
 	for i in range(number_dfs):
+		t0 = time.time()
 		# Load the original dfs
+		# print("Loading original dataframe {0}".format(t1-t0))
 		file_path = file_path_base + str(i) + file_path_end
+		print("Loading " + file_path)
 		df = dm.load_pickled_df(directory_path, file_path)
 
 		# Adjust the df
+		print("Processing dataframe")
+		df.drop_duplicates(subset = df.columns[:-1],inplace=True)
 		df = dm.df_column_reduce(df)
 		df = dm.df_title_format(df)
-		df = dm.df_review_collapse(df)
 
-		# Pickle and save the adjust df
-		pickle.dump( df, open( file_path, "wb"))
+		# Concat the dataframe with each new df loaded
+		print("Adding df to df_all with concat")
+		if i == 0:
+			df_all = df.copy()
 
-if __name__ == '__main__':
-	main()
+		else:
+			df_all = pd.concat([df_all, df])
+
+		t1 = time.time()
+		print("Time iteration {0} was {1:.2f} seconds".format( i, (t1-t0) ))
+
+	# Pickle and save the new, compiled df
+	new_file = 'df_reviews_all.pandas'
+	print("Saving pickled dataframe as " + new_file)
+	new_file_path = directory_path + new_file
+	t0 = time.time()
+	pickle.dump( df_all, open( new_file_path, "wb"))
+	t1 = time.time()
+	print("Time to save pickled all was {0:.2f}".format(t1-t0))
+
+	# Create and save the gensime Doc2Vec model
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+	reviews = LabeledReviewSentence(df_all)
+
+	num_workers = 4 # 4 on my macbook pro, may need to adjust for running on AWS
+	t0 = time.time()
+	model = gensim.models.Doc2Vec(reviews, workers=num_workers)
+	t1 = time.time()
+	print("Time to run Doc2Vec to generate model was {0:.2f}".format(t1-t0))
+
+	# Save the model
+	model_directory = '../doc2vec_models/'
+	model_filename = 'model_all_01.doc2vec'
+	t0 = time.time()
+	model.save(model_directory+model_filename)
+	t1 = time.time()
+	print("Time to save Doc2Vec model was {0:.2f}".format(t1-t0))
+	
---
+++ doc2vec_main_02.py
@@ -0,0 +1,90 @@
+"""
+Main function to implement Doc2Vec.
+
+Now recognize the structure of the original dfs.
+
+Part of Project for Insight Data Science - Silicon Valley 2015a
+
+David L. Hochstetler
+1/20/15
+
+Much thanks to Matt P. and Ben E. for the dataframe and Doc2Vec help!
+"""
+
+import doc2vec_methods as dm
+import scraping_methods as scrape
+from doc2vec_classes import LabeledReviewSentence
+import pdb					# For debugging
+import sys
+import re
+import gensim, logging 		# For Doc2Vec
+import time 				# For timing
+
+
+if __name__ == '__main__':
+	# Set the path to the pickled file
+	directory_path = '../../Lemmatized_by_Sentence/'
+	file_path = 'amazon_music_random_lemmatized_0.pandas'
+
+	t0 = time.time()
+	df = dm.load_pickled_df(directory_path, file_path)
+	t1 = time.time()
+	print("Time to run load_pickled_df was {0}".format(t1-t0))
+
+	# # Smaller to help with debugging (1000,10000,100000)
+	# df = df_full[:10000].copy(deep=True)
+
+	# Get the df into desired format
+	# NOTE: This assumes set/hard-coded columns and column names in each df
+	t0 = time.time()
+	df = dm.df_column_reduce(df)
+	t1 = time.time()
+	print("Time to run df_column_reducer was {0}".format(t1-t0))
+
+	t0 = time.time()
+	df = dm.df_title_format(df)
+	t1 = time.time()
+	print("Time to run df_title_format was {0}".format(t1-t0))
+
+	# pdb.set_trace()
+
+	t0 = time.time()
+	# df = dm.df_review_collapse(df)
+	df.drop_duplicates(subset = df.columns[:-1], inplace=True)
+	t1 = time.time()
+	print("Time to run df_review_collapse was {0}".format(t1-t0))
+
+	# Run Doc2Vec on the dataset of reviews
+	# Not sure what this does...
+	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+	reviews = LabeledReviewSentence(df)
+
+	# pdb.set_trace()
+	
+	# Instantiate the doc2vec model
+	# Want to know how long this training takes
+	# Need to look into adjustable parameters...
+	# --> SEE gensim site
+	num_workers = 4 # 4 on my macbook pro, may need to adjust for running on AWS
+	t0 = time.time()
+	model = gensim.models.Doc2Vec(reviews, workers=num_workers)
+	t1 = time.time()
+	print("Time to run Doc2Vec to generate model was {0}".format(t1-t0))
+
+	# # Look at results
+	# positive_terms = ['hard','rock']
+	# negative_terms = []
+	# print model.most_similar_cosmul(positive= positive_terms, 
+	# 	negative=negative_terms, topn=10)
+
+	# # Check out only titles:
+	# title_scores = dm.most_similar_titles(df,positive_terms,negative_terms,model)
+
+
+
+
+
+
+
+
