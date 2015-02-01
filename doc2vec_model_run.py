6f830825850f72fe90ff70576ff81ca00fd14bfe
David Hochstetler <dlhochstetler@gmail.com>
2015-02-01 13:33:55 -0800

    new commit

--- doc2vec_model_run.py
+++ doc2vec_model_run.py
@@ -16,8 +16,8 @@ import cPickle as pickle
 import time
 import gensim, logging 		# For Doc2Vec
 import os
-import sys
-import pdb
+# import sys
+# import pdb
 
 if __name__ == '__main__':
 
@@ -43,40 +43,8 @@ if __name__ == '__main__':
 
 	# Build the model
 	num_workers = 4
-	num_epochs = 2
+	num_epochs = 10
 	
-	# pdb.set_trace()
-	# model_title = gensim.models.Doc2Vec(reviews_title, workers=num_workers, 
-	# 	alpha=0.025, min_alpha=0.025)
-
-	t0 = time.time()
-	model_artist = gensim.models.Doc2Vec(workers=num_workers, alpha=0.025, min_alpha=0.025)
-	model_artist.build_vocab(reviews_artist)
-	for epoch in range(num_epochs):
-		print epoch
-		model_artist.train(reviews_artist)
-		model_artist.alpha -= 0.002*5
-		model_artist.min_alpha = model_artist.alpha
-	# model_artist = gensim.models.Doc2Vec(reviews_artist, workers=num_workers, 
-	# 	alpha=0.025, min_alpha=0.025)
-	t1 = time.time()
-	print("Time to run Doc2Vec to generate artist model for {0} epochs was {1:.2f}".format(num_epochs,t1-t0))
-	model_artist.save(dir_path +'doc2vec_model_artist2.doc2vec')
-
-	pdb.set_trace()
-
-	t0 = time.time()
-	model_title_artist = gensim.models.Doc2Vec(workers=num_workers, alpha=0.025, min_alpha=0.025)
-	model_title_artist.build_vocab(reviews_title_artist)
-	for epoch in range(num_epochs):
-		model_title_artist.train(reviews_title_artist)
-		model_title_artist.alpha -= 0.002
-		model_title_artist.min_alpha = model_title_artist.alpha
-	# model_title_artist = gensim.models.Doc2Vec(reviews_title_artist, workers=num_workers, 
-	# 	alpha=0.025, min_alpha=0.025)
-	t1 = time.time()
-	print("Time to run Doc2Vec to generate title-artist model for {0} epochs was {1:.2f}".format(num_epochs,t1-t0))
-	model_title_artist.save(dir_path +'doc2vec_model_title_artist10.doc2vec')
 
 	t0 = time.time()
 	model_all = gensim.models.Doc2Vec(workers=num_workers, alpha=0.025, min_alpha=0.025)
@@ -85,11 +53,47 @@ if __name__ == '__main__':
 		model_all.train(reviews_all)
 		model_all.alpha -= 0.002
 		model_all.min_alpha = model_all.alpha
-	# model_all = gensim.models.Doc2Vec(reviews_all, workers=num_workers, 
-	# 	alpha=0.025, min_alpha=0.025)
 	t1 = time.time()
 	print("Time to run Doc2Vec to generate all model for {0} epochs was {1:.2f}".format(num_epochs,t1-t0))
 	model_all.save(dir_path +'doc2vec_model_all10.doc2vec')
+
+	t0 = time.time()
+	model_artist = gensim.models.Doc2Vec(workers=num_workers, alpha=0.025, min_alpha=0.025)
+	model_artist.build_vocab(reviews_artist)
+	for epoch in range(num_epochs):
+		print epoch
+		model_artist.train(reviews_artist)
+		model_artist.alpha -= 0.002
+		model_artist.min_alpha = model_artist.alpha
+	t1 = time.time()
+	print("Time to run Doc2Vec to generate artist model for {0} epochs was {1:.2f}".format(num_epochs,t1-t0))
+	model_artist.save(dir_path +'doc2vec_model_artist10.doc2vec')
+
+	# t0 = time.time()
+	# model_title_artist = gensim.models.Doc2Vec(workers=num_workers, alpha=0.025, min_alpha=0.025)
+	# model_title_artist.build_vocab(reviews_title_artist)
+	# for epoch in range(num_epochs):
+	# 	model_title_artist.train(reviews_title_artist)
+	# 	model_title_artist.alpha -= 0.002
+	# 	model_title_artist.min_alpha = model_title_artist.alpha
+	# # model_title_artist = gensim.models.Doc2Vec(reviews_title_artist, workers=num_workers, 
+	# # 	alpha=0.025, min_alpha=0.025)
+	# t1 = time.time()
+	# print("Time to run Doc2Vec to generate title-artist model for {0} epochs was {1:.2f}".format(num_epochs,t1-t0))
+	# model_title_artist.save(dir_path +'doc2vec_model_title_artist10.doc2vec')
+
+	# t0 = time.time()
+	# model_all = gensim.models.Doc2Vec(workers=num_workers, alpha=0.025, min_alpha=0.025)
+	# model_all.build_vocab(reviews_all)
+	# for epoch in range(num_epochs):
+	# 	model_all.train(reviews_all)
+	# 	model_all.alpha -= 0.002
+	# 	model_all.min_alpha = model_all.alpha
+	# # model_all = gensim.models.Doc2Vec(reviews_all, workers=num_workers, 
+	# # 	alpha=0.025, min_alpha=0.025)
+	# t1 = time.time()
+	# print("Time to run Doc2Vec to generate all model for {0} epochs was {1:.2f}".format(num_epochs,t1-t0))
+	# model_all.save(dir_path +'doc2vec_model_all10.doc2vec')
 
 
 
